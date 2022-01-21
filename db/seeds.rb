c08e538140cbef959d9e4fd4f4ee0f1a2e4ede1f
Lunarang <ecmettille@gmail.com>
2022-01-20 23:31:54 -0800

    added seeds for migration

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+require_relative "../config/environment.rb"
+
+mel = User.create(name: "Melissa", email: "melliebellie@gmail.com", password: "mellsbells")
+erin = User.create(name: "Erin", email: "erin.smells@gmail.com", password: "password")
+steve = User.create(name: "Steve", email: "steviewonder@gmail.com", password: "bucky12")
+
+Dog.create(name: "Dodger", sex: "Female", dob: "2019-11-30", user_id: "#{erin.id}")
+Dog.create(name: "Buccaroo", sex: "Male", dob: "2020-08-26", user_id: "#{steve.id}")
+Dog.create(name: "Goose", sex: "Male", dob: "2015-02-03", user_id: "#{mel.id}")

da6ea166179e7c3a57133e0f9f886cea742b588c
Lunarang <ecmettille@gmail.com>
2022-01-29 11:25:46 -0800

    deleted - reset database to rewrite good initial migrations

--- db/migrate/20220122230819_change_trackers_to_dogskills.rb
+++
@@ -1,6 +0,0 @@
-class ChangeTrackersToDogskills < ActiveRecord::Migration
-  def change
-    rename_table :trackers, :dog_skills
-    change_column :dog_skills, :level, :string, :default => "Novice"
-  end
-end

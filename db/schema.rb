0f02099099c12ec97b9f49abcd6a97c0a2202707
Lunarang <ecmettille@gmail.com>
2022-01-29 11:26:07 -0800

    reset database to rewrite good initial migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220122230819) do
+ActiveRecord::Schema.define(version: 20220121065641) do
 
   create_table "dog_skills", force: :cascade do |t|
     t.integer  "dog_id"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20220122230819) do
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.string   "sex"
-    t.text     "dob"
+    t.date     "dob"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

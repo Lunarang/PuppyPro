3e2f561248fef0ef17395527a20ee2fcca45b0be
Lunarang <ecmettille@gmail.com>
2022-01-29 11:20:26 -0800

    ActiveRecord::Base could not configure database, used 'configure :development' block instead

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,12 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure :development do
+  set :database, "sqlite3:db/#{ENV['SINATRA_ENV']}.db"
+end
 
 require 'rack-flash'
-require "date"
+require 'date'
 
 require './app/controllers/application_controller'
 

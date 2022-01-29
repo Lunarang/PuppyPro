53cbc2760f48ca2fbf4161a4e83440f8ab0543c6
Lunarang <ecmettille@gmail.com>
2022-01-29 10:40:09 -0800

    adjusted the logic for iterating through params hash to obtain and update skills/levels for new or updated dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -5,38 +5,50 @@ class DogsController < ApplicationController
         redirect "/home"
     end
 
-# New (Create)
+# New (Create.R.U.D) - GET
     get "/dogs/new" do
         login_required
+        current_user
 
         erb :"/dogs/new"
     end
 
+# New (Create.R.U.D) - POST
     post "/dogs/new" do
         login_required
         current_user
         @dog = Dog.new(params["dog"])
 
         if @dog.valid?
+            @dog.user_id = @user.id
             @dog.save
-            params["dogskill"].each do |skill_id|
+
+            params["dogskill"]["skill_ids"].each do |skill_id|
                 id = skill_id.to_i
-                DogSkill.find_by(skill_id: id, dog_id: @dog.id).update(level: params["dogskill"]["#{skill_id}"])
+                DogSkill.create(skill_id: id, dog_id: @dog.id)
+            end
+
+            params["dogskill"]["skill_lvls"].each do |skill_id, level|
+                if params["dogskill"]["skill_ids"].include?(skill_id)
+                    id = skill_id.to_i
+                    level = "novice" if level == "none"
+                    DogSkill.find_by(skill_id: id, dog_id: @dog.id).update(level: level)
+                end
             end
 
             if !params["skill"].empty?
                 new_skill = Skill.create(name: params["skill"]["name"], description: params["skill"]["description"], method: params["skill"]["method"])
                 DogSkill.create(skill_id: new_skill.id, dog_id: @dog.id, level: params["skill"]["level"])
             end
-            redirect '/home'
 
+            redirect '/home'
         else
             flash[:error] = @dog.errors.full_messages
-
+            redirect '/dogs/new'
         end
     end
   
-# Show (Read, Delete)
+# Show (C.Read.U.D)
     get "/dogs/:id" do
         current_user
         @dog = Dog.find(params[:id])
@@ -44,7 +56,7 @@ class DogsController < ApplicationController
         erb :"dogs/show"
     end
 
-# Edit (Update)
+# Edit (C.R.Update.D) - GET
     get "/dogs/:id/edit" do
         login_required
         @dog = Dog.find(params[:id])
@@ -57,14 +69,15 @@ class DogsController < ApplicationController
         end
     end
 
-    patch "/dogs/:id" do
+# Edit (C.R.Update.D) - PATCH
+    patch "/dogs/:id/edit" do
         login_required
 
         @dog = Dog.find(params[:id])
         @dog.update(name: params["dog"]["name"], sex: params["dog"]["sex"], dob: params["dog"]["dob"])
         
         skill_list = []
-        params["dog"]["skill_ids"].each do |skill_id|
+        params["dogskill"]["skill_ids"].each do |skill_id|
             id = skill_id.to_i
             skill_list << id
         end
@@ -74,12 +87,12 @@ class DogsController < ApplicationController
             skill = Skill.find_by(id: id)
             dogskill = DogSkill.find_by(skill_id: id, dog_id: @dog.id)
 
-            if !skill_list.include?(skill.id) && @dog.skills.include?(skill)
+            if !skill_list.include?(id) && @dog.skills.include?(skill)
                 dogskill.destroy_all
-            elsif skill_list.include?(skill.id) && @dog.skills.include?(skill)
-                dogskill.update(level: params["dogskill"]["#{skill_id}"])
+            elsif skill_list.include?(id) && @dog.skills.include?(skill)
+                dogskill.update(level: params["dogskill"]["skill_id"])
             elsif !@dog.skills.include?(skill)
-                DogSkill.create(skill_id: skill.id, dog_id: @dog.id, level: params["dogskill"]["#{skill_id}"])
+                DogSkill.create(skill_id: id, dog_id: @dog.id, level: params["dogskill"]["skill_id"])
             end
 
         end
@@ -94,7 +107,7 @@ class DogsController < ApplicationController
         redirect to "/dogs/#{@dog.id}"
     end
 
-# Delete (Destroy)
+# Delete (C.R.U.Destroy)
     delete "/dogs/:id" do
         @dog = Dog.find_by(id: params[:id])
         @dog.destroy_all

edaab10ec0a0cf438b6c7365bf54abb9256955d9
Lunarang <ecmettille@gmail.com>
2022-01-29 13:33:14 -0800

    refactored application controller helper methods for DRYer controllers when verifying user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,61 +13,69 @@ class UsersController < ApplicationController
 # Show (C.Read.U.D)
     get "/users/:id" do
         current_user
-        @user = User.find(params[:id])
+        @shown_user = User.find(params[:id])
 
         erb :"users/show"
     end
 
 # Edit (C.R.Update.D) - GET
     get "users/:id/edit" do
-        login_required
-        user = User.find(params[:id])
+        verify_user_login
+        @shown_user = User.find(params[:id])
 
-        if current_user.id == user.id
+        if @user.id == @shown_user.id
             erb :"/skills/edit"
         else
             flash[:notice] = "You can only edit your own profile!"
-            redirect to "/home"
+            redirect to "/users/#{@shown_user.id}"
         end
     end
 
 # Edit (C.R.Update.D) - PATCH
     patch "/users/:id" do
-        login_required
+        verify_user_login
+        @shown_user = User.find(params[:id])
 
-        current_user.update(params["user"])
-        
-        skill_list = []
-        params["userskills"]["skill_ids"].each do |skill_id|
-            id = skill_id.to_i
-            skill_list << id
-        end
-
-        skill_list.each do |id|
-        
-            skill = Skill.find_by(id: id)
-
-            if !skill_list.include?(skill.id) && current_user.skills.include?(skill)
-                UserSkill.find_by(skill_id: id, user_id: current_user.id).destroy_all
-            elsif !current_user.skills.include?(skill)
-                UserSkill.create(skill_id: skill.id, user_id: current_user.id)
+        if @user.id == @shown_user.id
+            @user.update(params["user"])
+            
+            skill_list = []
+            params["userskills"]["skill_ids"].each do |skill_id|
+                id = skill_id.to_i
+                skill_list << id
             end
 
+            skill_list.each do |id|
+            
+                skill = Skill.find_by(id: id)
+
+                if !skill_list.include?(skill.id) && @user.skills.include?(skill)
+                    UserSkill.find_by(skill_id: id, user_id: @user.id).destroy_all
+                elsif !@user.skills.include?(skill)
+                    UserSkill.create(skill_id: skill.id, user_id: @user.id)
+                end
+
+            end
+        else
+            flash[:notice] = "You can only edit your own profile!"
         end
 
-        redirect to "/home"
+        redirect to "/users/#{@shown_user.id}"
     end
 
 # Delete (C.R.U.Destroy)
     delete "/user/:id" do
-        login_required
-        user = User.find(params[:id])
+        verify_user_login
+        @shown_user = User.find(params[:id])
 
-        if current_user.id == user.id
+        if @user.id == @shown_user.id
             user.destroy_all
+            redirect to "/"
+        else
+            flash[:notice] = "You can only delete your own account!"
+            redirect to "/users/#{@shown_user.id}"
         end
-
-        redirect to "/welcome"
     end
+
 end
         

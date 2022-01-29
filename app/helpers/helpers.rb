63b8b572de1f9b967efb6049ba475fe1bd288a8f
Lunarang <ecmettille@gmail.com>
2022-01-29 12:32:25 -0800

    defined helper method that confirms if dog/skill belongs to currently logged in user

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,11 +3,15 @@ require './config/environment'
 class Helpers
 
   def self.current_user(session)
-    @user = User.find_by_id(session[:user_id])
+    User.find_by_id(session[:user_id])
   end
 
-  def self.is_logged_in?(session)
+  def self.logged_in?(session)
     !!session[:user_id]
   end
-  
+
+  def self.belong_to_user?(session)
+    Helpers.logged_in?(session) == true && session.id == Helpers.current_user(session).id
+  end
+
 end
